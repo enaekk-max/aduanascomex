242e3fa574ee256b39e018d01ab9c36f60c3a0b0
enaekk-max <enaekk@gmail.com>
2026-05-22 09:29:42 -0400

    Add files via upload

---
+++ pagos-ms/pagos-ms/pagos-ms/pagos-ms/src/main/java/com/aduanas/com/pagosms/controller/PagoController.java
@@ -0,0 +1,58 @@
+package com.aduanas.com.pagosms.controller;
+
+import com.aduanas.com.pagosms.dto.PagoRequestDto;
+import com.aduanas.com.pagosms.dto.PagoResponseDto;
+import com.aduanas.com.pagosms.dto.NotificacionBancoDto;
+import com.aduanas.com.pagosms.service.PagoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pagos")
+@RequiredArgsConstructor
+public class PagoController {
+
+    private final PagoService pagoService;
+
+    // ==========================================
+    // POST: REGISTRAR INTENTO DE PAGO (PENDIENTE)
+    // ==========================================
+    @PostMapping
+    public ResponseEntity<PagoResponseDto> registrarPago(@Valid @RequestBody PagoRequestDto requestDto) {
+        PagoResponseDto respuesta = pagoService.procesarPago(requestDto);
+        return new ResponseEntity<>(respuesta, HttpStatus.CREATED); // Retorna 201 Created
+    }
+
+    // ==========================================
+    // POST: WEBHOOK SIMULADOR DEL BANCO (COMPLETADO / FALLIDO)
+    // ==========================================
+    @PostMapping("/notificacion-banco")
+    public ResponseEntity<PagoResponseDto> recibirNotificacionBanco(@Valid @RequestBody NotificacionBancoDto bancoDto) {
+        // Modifica el estado del pago y gatilla la liberación remota en carga-ms
+        PagoResponseDto respuesta = pagoService.confirmarPagoDesdeBanco(bancoDto);
+        return ResponseEntity.ok(respuesta); // Retorna 200 OK
+    }
+
+    // ==========================================
+    // GET: LISTAR TODOS LOS PAGOS
+    // ==========================================
+    @GetMapping
+    public ResponseEntity<List<PagoResponseDto>> listarTodos() {
+        List<PagoResponseDto> pagos = pagoService.obtenerTodosLosPagos();
+        return ResponseEntity.ok(pagos);
+    }
+
+    // ==========================================
+    // GET: BUSCAR PAGO POR ID
+    // ==========================================
+    @GetMapping("/{id}")
+    public ResponseEntity<PagoResponseDto> buscarPorId(@PathVariable Long id) {
+        PagoResponseDto pago = pagoService.obtenerPagoPorId(id);
+        return ResponseEntity.ok(pago);
+    }
+}
